cc7663c9ce51f42fd7956ce4d687ae5d05743c92
conandoyled <asmarek@coe.edu>
2017-02-09 10:52:25 -0600

    Fab goes to second activity, Added New Theme

---
+++ app/src/main/java/edu/coe/asmarek/mybookshelf/AddBookText.java
@@ -0,0 +1,13 @@
+package edu.coe.asmarek.mybookshelf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddBookText extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book_text);
+    }
+}
